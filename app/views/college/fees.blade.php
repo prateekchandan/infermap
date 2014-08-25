18d494fbb98e4205c3853755bb88947141a82421
prateekchandan <prateekchandan5545@gmail.com>
2014-08-25 23:48:48 +0530

    updated dummy fee

--- app/views/college/fees.blade.php
+++ app/views/college/fees.blade.php
@@ -2,8 +2,8 @@
 
 @section('college-content')
 <?php  $check=0; ?>
-<div class="row">
 @if($fees_table!=0)
+<div class="row">
 <?php  $check=1;?>
 <div class="col-md-9" style="overflow-x:auto">
 <div class="title">
@@ -39,7 +39,7 @@
 		@for( $j=1 ; $j < sizeof($fees_table[$i]);$j++)
 			@if($fees_table[$i][$j]!='-')
 				<li>
-					{{$fees_table[0][$j]}} => {{$fees_table[$i][$j]}}
+					{{$fees_table[0][$j]}} : {{$fees_table[$i][$j]}}
 				</li>
 			@endif
 		@endfor
@@ -51,7 +51,6 @@
 	
 	
 </div>
-@endif
 <div class="col-md-3">
 	<div class="title"><h3>Ratings</h3></div>
 		<ul class="skills">
@@ -66,6 +65,54 @@
 		</ul>
 </div>
 </div>
+@endif
+
+@if($fees_table==0)
+<div class="row">
+	<div class="col-md-9">
+		<div class="title">
+			<h3>
+				Fee Structure Per
+				@if($data['fee_type']==0)
+				Semester
+				@else
+				Annum
+				@endif
+				(in ₹)
+			</h3>
+		</div>
+
+		<div>
+			Following table contains Fee structure 
+			in <a href="{{URL::to('college')}}/{{$data['link']}}">{{$data['name']}}</a>
+		</div>
+		<ul>
+			<li><b>B.Tech/B.E.</b>
+				<ul class="arrow_list">
+					<li>Tution Fee : N/A</li>
+					<li>Total : N/A</li>
+					<li>Refundable fee : N/A</li>
+				</ul>
+			</li>
+		</ul>
+		
+	</div>
+	<div class="col-md-3">
+	<div class="title"><h3>Ratings</h3></div>
+		<ul class="skills">
+			<li>
+				<h5>Difficulty in admission ( 40% )</h5>
+				<div class="meter"><span style="width: 40%"></span></div><!-- Edite width here -->
+			</li>
+			<li>
+				<h5>Wordpress ( 80% )</h5>
+				<div class="meter"><span style="width: 80%"></span></div><!-- Edite width here -->
+			</li>
+		</ul>
+</div>
+	
+</div>
+@endif
 
 @if(File::exists(public_path().'/data'.'/'.$data['cid'].'/fees/scholarships.txt'))
 @if(trim(file_get_contents(public_path().'/data'.'/'.$data['cid'].'/fees/scholarships.txt'))!='')
@@ -115,9 +162,7 @@
 @endif
 @endif
 
-@if($check==0)
-@include('college.nodata')
-@endif
+
 	
 
 @endsection
