96fc38e36fdee23964958b54455847ace7dff5f6
prateekchandan <prateekchandan5545@gmail.com>
2014-09-06 06:25:28 +0530

    nodata

--- app/views/exams/syllabus.blade.php
+++ app/views/exams/syllabus.blade.php
@@ -2,11 +2,12 @@
 @section('exam-content')
 
 <?php  $check=0; ?>
-
+@if($exam->syllabus!='')
+<?php  $check=0; ?>
 <div class="row">
 	<div class="col-md-12">
 		<div class="title">
-			<h3>Exam Patten</h3>
+			<h3>Syllabus</h3>
 		</div>
 		<div class="row col-md-12">
 			<div class="editable-content">
@@ -15,5 +16,9 @@
 		</div>
 	</div>
 </div>
+@indif
 
+@if($check==0)
+	@include('college.nodata')
+@endif
 @endsection
