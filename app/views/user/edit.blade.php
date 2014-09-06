3a823c238035459c8c0437304c3c06d0667d287e
prateekchandan <prateekchandan5545@gmail.com>
2014-09-06 06:48:25 +0530

    user update page message bag correct

--- app/views/user/edit.blade.php
+++ app/views/user/edit.blade.php
@@ -21,7 +21,7 @@
 <div class="container">
 
 @if (Session::get('messages') != null && Session::get('messages')->has('home_message'))
-<div class="col-md-12">
+<div class="col-md-12 row">
 	<div class="alert alert-success alert-dismissible" role="alert" style="position:absolute;top:50px; width:50%; left:25%;opacity:0.6">
 	<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
 	{{ Session::get('messages')->first('home_message') }}
